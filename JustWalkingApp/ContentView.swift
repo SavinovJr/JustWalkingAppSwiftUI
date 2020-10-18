7849dade057095cf7bfcbe23d240e92f3c603197
Anton Savinov <Anton.Savinov@Harman.com>
2020-10-18 15:05:23 +0300

    Initial Commit

---
+++ JustWalkingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustWalkingApp
+//
+//  Created by Антон Савинов on 18.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JustWalkingApp/JustWalkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkingAppApp.swift
+//  JustWalkingApp
+//
+//  Created by Антон Савинов on 18.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
